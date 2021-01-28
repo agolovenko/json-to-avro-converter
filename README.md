3fe180451144647da7f2f6fe15f963c4f9e69783
agolovenko <ashotik@gmail.com>
2021-01-28 14:30:28 +0100

    initial version (#1)
    * added files
    
    * updated readme
    
    Co-authored-by: Ashot <ashot.golovenko@scrm.eu>

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # json-to-avro-converter
-a converter of JSON to AVRO written in Scala
+Converter of JSON format to AVRO `GenericData.Record` written in Scala using Play-Json
+
+## JSON2Avro Converter
+
+### Features
+
+* unions without avro wrapping
+* schema's default values are used if data is missing
+* data not present in schema is ignored
+* currently not supporting `BYTES` and `FIXED` types 
+
+### Usage
+
+```scala
+import agolovenko.avro._
+import org.apache.avro.generic.GenericData
+import org.apache.avro.Schema
+import play.api.libs.json.Json
+
+import Schema._
+
+val schema = new Parser().parse("""
+     |{
+     |  "type": "record",
+     |  "name": "sch_rec2",
+     |  "fields": [
+     |    {
+     |     "name": "field2", 
+     |     "type": {
+     |       "type": "array",
+     |       "items": "int"
+     |     },
+     |     "default": [1, 2, 3]
+     |    }
+     |  ]
+     |}""".stripMargin)
+
+val data   = Json.parse("""{"field1": [12, 14]}""")
+val record = new AvroParser().parse(data, schema)
+val bytes  = toBytes(record)
+```
+For more examples check out the tests!
---
+++ src/test/scala/agolovenko/avro/NestedRecordArraySpec.scala
@@ -0,0 +1,70 @@
+package agolovenko.avro
+
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
+import org.apache.avro.reflect.ReflectData
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.Json
+
+import scala.collection.JavaConverters._
+
+class NestedRecordArraySpec extends AnyWordSpec with Matchers {
+  import Schema._
+
+  private val schema = new Parser().parse("""
+      |{
+      |  "type": "record",
+      |  "name": "sch_rec1",
+      |  "fields": [
+      |    {
+      |     "name": "field1",
+      |     "type": {
+      |       "name": "sch_n_arr2",
+      |       "type": "array",
+      |       "items": {
+      |         "name": "sch_n_rec2",
+      |         "type": "record",
+      |         "fields": [
+      |           {
+      |             "name": "n_field1",
+      |             "type": "string"
+      |           },
+      |           {
+      |             "name": "n_field2",
+      |             "type": "int"
+      |           }
+      |          ]
+      |        }
+      |      },
+      |      "default": [{"n_field1": "bbb", "n_field2": 33}]
+      |    }
+      |  ]
+      |}""".stripMargin)
+
+  "parses nested record array correctly" in {
+    val data   = Json.parse("""{"field1": [{"n_field1": "aaa", "n_field2": 23}]}""")
+    val record = new AvroParser().parse(data, schema)
+
+    ReflectData.get().validate(schema, record) should ===(true)
+
+    val rec1 = new GenericData.Record(schema.getField("field1").schema().getElementType)
+    rec1.put("n_field1", "aaa")
+    rec1.put("n_field2", 23)
+    val expected = new GenericData.Array(schema.getField("field1").schema(), Seq(rec1).asJava)
+    record.get("field1") should ===(expected)
+  }
+
+  "applies default value to nested record array" in {
+    val data   = Json.parse("{}")
+    val record = new AvroParser().parse(data, schema)
+
+    ReflectData.get().validate(schema, record) should ===(true)
+
+    val rec1 = new GenericData.Record(schema.getField("field1").schema().getElementType)
+    rec1.put("n_field1", "bbb")
+    rec1.put("n_field2", 33)
+    val expected = new GenericData.Array(schema.getField("field1").schema(), Seq(rec1).asJava)
+    record.get("field1") should ===(expected)
+  }
+}
